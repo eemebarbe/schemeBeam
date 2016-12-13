8bce56df336afb16e93ab17a4f7050f843b92b66
Jeremy Barbe <littlejssn@gmail.com>
2016-12-13 02:34:50 -0700

    finished addemail api endpoint

--- api/api.js
+++ api/api.js
@@ -13,42 +13,49 @@ module.exports = function(app, connection) {
             var name = email.substring(0, email.lastIndexOf("@"));
             var domain = email.substring(email.lastIndexOf("@") +1);
             var hashCode = md5(name + domain);
-            var referredBy;
-            if(req.body.hashCode !== undefined) {
-                connection.query('SELECT emailaddress FROM emails WHERE `referralcode`=(?)', [req.body.hashCode], function(err, rows, fields) {
-                    if(err) throw err;
-                    referredBy = rows[0].emailaddress;
+
+            function sendGrid(referredBy){
+                connection.query('INSERT INTO emails (emailaddress, referralcode, referredby) VALUES (?, ?, ?)', [req.body.email, hashCode, referredBy], function(err, rows, fields) {
+                    if (err) {
+                        res.status(401);
+                    } else {
+                        var helper = require('sendgrid').mail;
+                        var from = new helper.Email("schemeBeam@schemeBeam.com");
+                        var to = new helper.Email(req.body.email);
+                        var subject = "subject here";
+                        var content = new helper.Content(
+                                "text/html", "<h1>Your referral link:</h1> <h2>" + req.body.domain + "/#/" + hashCode + "</h2>" +
+                                "<div><a href=\"" + req.body.domain + "/#/verify/" + hashCode + "\">Click here</a> to activate your referral link and to get started sharing with your contacts! Good luck!</div>" +
+                                "<br><div>If you would like to check on your position in the referral contest, <a href=\"" + req.body.domain + "/#/stats/" + hashCode + "\">click here.</a></div>");
+                        var mail = new helper.Mail(from, subject, to, content);
+
+                        var sg = require('sendgrid')(SENDGRID_API_KEY);
+
+                        var request = sg.emptyRequest({
+                          method: 'POST',
+                          path: '/v3/mail/send',
+                          body: mail.toJSON(),
+                        });
+
+                        function sendMessage() {
+                            sg.API(request, function(error, response) {
+                              // Handle the response here.
+                            });
+                        }
+                        sendMessage();
+                    }
                 });
             }
-            connection.query('INSERT INTO emails (emailaddress, referralcode, referredby) VALUES (?, ?, ?)', [req.body.email, hashCode, referredBy], function(err, rows, fields) {
-                if (err) {
-                    res.status(401);
-                } else {
-                    var helper = require('sendgrid').mail;
-                    var from = new helper.Email("schemeBeam@schemeBeam.com");
-                    var to = new helper.Email(req.body.email);
-                    var subject = "subject here";
-                    var content = new helper.Content(
-                            "text/html", "<h1>Your referral link:</h1> <h2>" + req.body.domain + "/#/" + hashCode + "</h2>" +
-                            "<div><a href=\"" + req.body.domain + "/#/verify/" + hashCode + "\">Click here</a> to activate your referral link and to get started sharing with your contacts! Good luck!</div>");
-                    var mail = new helper.Mail(from, subject, to, content);
-
-                    var sg = require('sendgrid')(SENDGRID_API_KEY);
-
-                    var request = sg.emptyRequest({
-                      method: 'POST',
-                      path: '/v3/mail/send',
-                      body: mail.toJSON(),
+            //pass hash code to select the user who gets a referral point
+                if(req.body.hashCode !== undefined){
+                    connection.query('SELECT emailaddress FROM emails WHERE `referralcode`=(?)', [req.body.hashCode], function(err, rows, fields) {
+                        if(err) throw err;
+                        var data = rows[0].emailaddress;
+                        sendGrid(data);
                     });
-
-                    function sendMessage() {
-                        sg.API(request, function(error, response) {
-                          // Handle the response here.
-                        });
-                    }
-                    sendMessage();
+                } else {
+                    sendGrid("none");
                 }
-            });
         }
         res.end();
     });
